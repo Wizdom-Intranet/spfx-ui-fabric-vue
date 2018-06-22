a73d953d5284e4b972c49ef18635251da2678393
Bo S. Christensen <bsc@webtop.dk>
2018-06-22 20:44:01 +0200

    docs now mounts vue components

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1,7 +1,9 @@
 import '@microsoft/load-themed-styles';
 
-var button = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('h1',[_vm._v("This is a button2 ")])},staticRenderFns: [],
-
+var uiButton = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('h1',[_vm._v(" this is button "),_c('input',{attrs:{"type":"text"},domProps:{"value":_vm.count}}),_c('button',{on:{"click":function($event){_vm.count++;}}},[_vm._v("add")])])},staticRenderFns: [],
+    data: function data(){
+        return { count : 1}
+    }
 }
 
-export { button };
+export { uiButton };
---
+++ src/components/uiButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>
+        this is button
+        <input type="text" :value="count" /><button @click="count++">add</button>
+    </h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return { count : 1}
+    }
+}
+</script>
+
+<style>
+
+</style>
