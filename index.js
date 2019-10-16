7b13becec9006d3dd880b16ff0d67b7dd0917b5d
Bo S. Pluszek <bsc@webtop.dk>
2019-10-16 07:15:21 +0200

    relocated index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+export { default as uiButton } from "./src/components/uiButton.vue";
+export { default as uiCallout } from "./src/components/uiCallout.vue";
+export { default as uiCheckbox } from "./src/components/uiCheckbox.vue";
+export { default as uiChoiceField } from "./src/components/uiChoiceField.vue";
+export { default as uiChoiceFieldGroup } from "./src/components/uiChoiceFieldGroup.vue";
+export { default as uiContextualMenu } from "./src/components/uiContextualMenu.vue";
+export { default as uiContextualMenuItem } from "./src/components/uiContextualMenuItem.vue";
+export { default as uiCommandBar } from "./src/components/uiCommandBar.vue";
+export { default as uiCommandButton } from "./src/components/uiCommandButton.vue";
+export { default as uiDialog } from "./src/components/uiDialog.vue";
+export { default as uiDropdown } from "./src/components/uiDropdown.vue";
+export { default as uiDropdownItem } from "./src/components/uiDropdownItem.vue";
+export { default as uiLabel } from "./src/components/uiLabel.vue";
+export { default as uiLink } from "./src/components/uiLink.vue";
+export { default as uiList } from "./src/components/uiList.vue";
+export { default as uiListActions } from "./src/components/uiListActions.vue";
+export { default as uiListActionItem } from "./src/components/uiListActionItem.vue";
+export { default as uiListItem } from "./src/components/uiListItem.vue";
+export { default as uiMessageBar } from "./src/components/uiMessagebar.vue";
+export { default as uiMessageBanner } from "./src/components/uiMessageBanner.vue";
+export { default as uiOverlay } from "./src/components/uiOverlay.vue";
+export { default as uiPanel } from "./src/components/uiPanel.vue";
+export { default as uiPersona } from "./src/components/uiPersona.vue";
+export { default as uiPivot } from "./src/components/uiPivot.vue";
+export { default as uiPivotItem } from "./src/components/uiPivotItem.vue";
+export { default as uiProgressIndicator } from "./src/components/uiProgressIndicator.vue";
+export { default as uiSearchBox } from "./src/components/uiSearchbox.vue";
+export { default as uiSpinner } from "./src/components/uiSpinner.vue";
+export { default as uiTextfield } from "./src/components/uiTextfield.vue";
+export { default as uiToggle } from "./src/components/uiToggle.vue";
+export { default as uiDynamicIcon } from "./src/components/uiDynamicIcon.vue";
+
+// our own components
+export { default as uiTimePicker } from "./src/components/uiTimePicker.vue";
+export { default as uiDatePicker } from "./src/components/uiDatePicker/uiDatePicker.vue";
+export { default as uiTypeAhead } from "./src/components/uiTypeAhead.vue";
+
+export *  from "./src/components/uiIcon";
