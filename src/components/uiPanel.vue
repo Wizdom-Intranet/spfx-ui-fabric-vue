6fb7cff2f46c4cd66279191ef7cf4226102c355d
Bo S. Pluszek <bsc@webtop.dk>
2019-11-27 10:23:07 +0100

    added svg for close icon in panel

--- src/components/uiPanel.vue
+++ src/components/uiPanel.vue
@@ -36,4 +36,8 @@ export default {
         border-top:solid 40px transparent;
         box-sizing: border-box;
     }
+    .ms-Icon.ms-Icon--Cancel
+    {
+        content: url('data:image/svg+xml;utf8,<svg viewBox="0 -0 2048 2048" width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M1115 1024l690 -691l-90 -90l-691 690l-691 -690l-90 90l690 691l-690 691l90 90l691 -690l691 690l90 -90z" fill="black" stroke="none"/></svg>');
+    }
 </style>
